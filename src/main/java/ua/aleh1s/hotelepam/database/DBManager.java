5233c5026513a19857cdddca33b2f56b35ee0c4c
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-12-12 19:02:48 +0200

    add logs for DBManager

--- src/main/java/ua/aleh1s/hotelepam/database/DBManager.java
+++ src/main/java/ua/aleh1s/hotelepam/database/DBManager.java
@@ -2,6 +2,8 @@ package ua.aleh1s.hotelepam.database;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,9 +15,11 @@ import static java.util.Objects.nonNull;
 import static ua.aleh1s.hotelepam.Constant.DATABASE_PROPERTIES;
 
 public class DBManager {
+    private static final Logger LOGGER = LogManager.getLogger(DBManager.class);
     private static DBManager instance;
     private HikariDataSource dataSource;
     private DBManager() {
+        LOGGER.trace("{} data source initialization...", DBManager.class.getName());
         ClassLoader classLoader = DBManager.class.getClassLoader();
         Optional<InputStream> optionalInputStream
                 = Optional.ofNullable(classLoader.getResourceAsStream(DATABASE_PROPERTIES));
@@ -26,9 +30,10 @@ public class DBManager {
                 HikariConfig config = new HikariConfig(properties);
                 this.dataSource = new HikariDataSource(config);
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.fatal(e.getMessage());
             }
         }
+        LOGGER.trace("{} data source initialized", DBManager.class.getName());
     }
     public synchronized static DBManager getInstance() {
         if (isNull(instance))
@@ -36,8 +41,10 @@ public class DBManager {
         return instance;
     }
     public synchronized static void shutdown() {
+        LOGGER.trace("{} shutdown is invoked", DBManager.class.getName());
         if (nonNull(instance))
             instance.getDataSource().close();
+        LOGGER.trace("{} shutdown is completed", DBManager.class.getName());
     }
     public HikariDataSource getDataSource() {
         return dataSource;
