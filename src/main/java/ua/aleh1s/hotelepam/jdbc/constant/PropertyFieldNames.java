ac576dd9228f9042f230d930bb43af860d8f4871
Oleksandr Palamarchuk <sashap740@gmail.com>
2023-01-26 16:32:20 +0200

    some refactoring

--- src/main/java/ua/aleh1s/hotelepam/jdbc/constant/PropertyFieldNames.java
+++
@@ -1,9 +0,0 @@
-package ua.aleh1s.hotelepam.jdbc.constant;
-
-public class PropertyFieldNames {
-
-    public static final String JDBC_URL = "jdbcUrl";
-    public static final String USERNAME = "username";
-    public static final String PASSWORD = "password";
-
-}
--- src/main/java/ua/aleh1s/hotelepam/model/mapper/impl/SqlUserEntityMapper.java
+++ src/main/java/ua/aleh1s/hotelepam/model/mapper/impl/SqlUserEntityMapper.java
@@ -1,41 +1,29 @@
-package ua.aleh1s.hotelepam.model.mapper.sql.impl;
+package ua.aleh1s.hotelepam.model.mapper.impl.sql.impl;
 
-import ua.aleh1s.hotelepam.model.constant.SqlFieldName;
 import ua.aleh1s.hotelepam.model.entity.UserEntity;
 import ua.aleh1s.hotelepam.model.entity.role.UserRole;
-import ua.aleh1s.hotelepam.model.mapper.sql.SqlEntityMapper;
-import ua.aleh1s.hotelepam.model.mapper.sql.exception.SqlEntityMapperException;
+import ua.aleh1s.hotelepam.model.mapper.exception.EntityMapperException;
+import ua.aleh1s.hotelepam.model.mapper.impl.sql.SqlEntityMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.ZoneId;
-import java.util.*;
+import java.util.Locale;
+import java.util.Optional;
 
 import static ua.aleh1s.hotelepam.model.constant.SqlFieldName.*;
 
 public class SqlUserEntityMapper implements SqlEntityMapper<UserEntity> {
 
     @Override
-    public Optional<UserEntity> mapOne(ResultSet resultSet) throws SqlEntityMapperException {
+    public Optional<UserEntity> map(ResultSet resultSet) throws EntityMapperException {
         try {
             if (resultSet.next())
                 return Optional.of(buildUserEntity(resultSet));
         } catch (SQLException e) {
-            throw new SqlEntityMapperException(e);
+            throw new EntityMapperException(e);
         }
         return Optional.empty();
-    }
-
-    @Override
-    public List<UserEntity> mapAll(ResultSet resultSet) throws SqlEntityMapperException {
-        List<UserEntity> result = new ArrayList<>();
-        try {
-            while (resultSet.next())
-                result.add(buildUserEntity(resultSet));
-        } catch (SQLException e) {
-            throw new SqlEntityMapperException(e);
-        }
-        return result;
     }
 
     private UserEntity buildUserEntity(ResultSet resultSet) throws SQLException {
