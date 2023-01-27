8067c11d3fa214f65de1f115fd62a443f19d7238
Oleksandr Palamarchuk <sashap740@gmail.com>
2023-01-27 09:35:01 +0200

    add UserRepository

---
+++ src/main/java/ua/aleh1s/hotelepam/model/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ua.aleh1s.hotelepam.model.repository;
+
+import ua.aleh1s.hotelepam.model.entity.UserEntity;
+
+import java.util.Optional;
+
+public interface UserRepository {
+
+    Optional<UserEntity> create(UserEntity userEntity);
+    Optional<UserEntity> findByLogin(String login);
+}
---
+++ src/main/java/ua/aleh1s/hotelepam/model/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,23 @@
+package ua.aleh1s.hotelepam.model.repository.impl;
+
+import ua.aleh1s.hotelepam.model.dao.impl.UserSimpleDao;
+import ua.aleh1s.hotelepam.model.entity.UserEntity;
+import ua.aleh1s.hotelepam.model.repository.UserRepository;
+
+import java.util.Optional;
+
+import static ua.aleh1s.hotelepam.model.repository.util.TransactionUtil.runInsideTransactionWithReturn;
+
+public class UserRepositoryImpl implements UserRepository {
+
+
+    @Override
+    public Optional<UserEntity> create(UserEntity userEntity) {
+        return runInsideTransactionWithReturn(dao -> dao.save(userEntity), new UserSimpleDao());
+    }
+
+    @Override
+    public Optional<UserEntity> findByLogin(String login) {
+        return runInsideTransactionWithReturn(dao -> dao.findBy(login).orElse(null), new UserSimpleDao());
+    }
+}
