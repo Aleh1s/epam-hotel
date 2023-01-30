c0f1533c4df84358e6aa73bacb12d55400ed46b4
Oleksandr Palamarchuk <sashap740@gmail.com>
2023-01-30 19:53:56 +0200

    update SqlUserEntityMapper

--- src/main/java/ua/aleh1s/hotelepam/model/constant/SqlFieldName.java
+++ src/main/java/ua/aleh1s/hotelepam/model/constant/SqlFieldName.java
@@ -7,6 +7,9 @@ public class SqlFieldName {
     // UserEntity
     public final static String USER_ID = "id";
     public final static String USER_EMAIL = "email";
+    public final static String USER_FIRST_NAME = "first_name";
+    public final static String USER_LAST_NAME = "last_name";
+    public final static String USER_PHONE_NUMBER = "phone_number";
     public final static String USER_PASSWORD = "password";
     public final static String USER_TIMEZONE = "timezone";
     public final static String USER_LOCALE = "locale";
